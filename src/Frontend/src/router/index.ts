017fcfae63753267e757dab76911056eb733058d
hbrunet <hectorbrunet@gmail.com>
2026-09-11 12:57:35 -0300

    Refactor router to use dynamic imports for views

--- src/Frontend/src/router/index.ts
+++ src/Frontend/src/router/index.ts
@@ -1,70 +1,100 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '../stores/authStore'
 import { tokenStorage } from '../utils/tokenStorage'
-import LoginView from '../views/LoginView.vue'
-import ConfiguracionesView from '../views/ConfiguracionesView.vue'
-import ConfiguracionDetailView from '../views/ConfiguracionDetailView.vue'
-import EscalasView from '../views/EscalasView.vue'
-import EscalaDetailView from '../views/EscalaDetailView.vue'
-import ValoresCategoriaView from '../views/ValoresCategoriaView.vue'
-import ValorCategoriaDetailView from '../views/ValorCategoriaDetailView.vue'
-import ValoresFijosView from '../views/ValoresFijosView.vue'
-import AsociacionMasivaValoresFijosView from '../views/AsociacionMasivaValoresFijosView.vue'
-import AsociacionMasivaValoresCategoriasView from '../views/AsociacionMasivaValoresCategoriasView.vue'
-import AsociacionMasivaConceptosView from '../views/AsociacionMasivaConceptosView.vue'
-import ClonacionMasivaValoresFijosView from '../views/ClonacionMasivaValoresFijosView.vue'
-import ClonacionMasivaValoresCategoriaView from '../views/ClonacionMasivaValoresCategoriaView.vue'
-import ActualizacionMasivaEscalaSalarialView from '../views/ActualizacionMasivaEscalaSalarialView.vue'
-import GruposValorFijoView from '../views/GruposValorFijoView.vue'
-import GruposValorCategoriaView from '../views/GruposValorCategoriaView.vue'
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/login', name: 'login', component: LoginView },
+    { path: '/login', name: 'login', component: () => import('../views/LoginView.vue') },
     { path: '/', redirect: '/configuraciones' },
-    { path: '/configuraciones', name: 'configuraciones', component: ConfiguracionesView },
-    { path: '/configuraciones/nueva', name: 'configuracion-nueva', component: ConfiguracionDetailView },
-    { path: '/configuraciones/:id', name: 'configuracion-detalle', component: ConfiguracionDetailView, props: true },
-    { path: '/escalas', name: 'escalas', component: EscalasView },
-    { path: '/escalas/nueva', name: 'escala-nueva', component: EscalaDetailView },
-    { path: '/escalas/:id', name: 'escala-detalle', component: EscalaDetailView },
-    { path: '/valores-categoria', name: 'valores-categoria', component: ValoresCategoriaView },
-    { path: '/valores-categoria/nuevo', name: 'valor-categoria-nuevo', component: ValorCategoriaDetailView },
-    { path: '/valores-categoria/:id', name: 'valor-categoria-detalle', component: ValorCategoriaDetailView },
-    { path: '/valores-fijos', name: 'valores-fijos', component: ValoresFijosView },
+    {
+      path: '/configuraciones',
+      name: 'configuraciones',
+      component: () => import('../views/ConfiguracionesView.vue'),
+    },
+    {
+      path: '/configuraciones/nueva',
+      name: 'configuracion-nueva',
+      component: () => import('../views/ConfiguracionDetailView.vue'),
+    },
+    {
+      path: '/configuraciones/:id',
+      name: 'configuracion-detalle',
+      component: () => import('../views/ConfiguracionDetailView.vue'),
+      props: true,
+    },
+    { path: '/escalas', name: 'escalas', component: () => import('../views/EscalasView.vue') },
+    {
+      path: '/escalas/nueva',
+      name: 'escala-nueva',
+      component: () => import('../views/EscalaDetailView.vue'),
+    },
+    {
+      path: '/escalas/:id',
+      name: 'escala-detalle',
+      component: () => import('../views/EscalaDetailView.vue'),
+    },
+    {
+      path: '/valores-categoria',
+      name: 'valores-categoria',
+      component: () => import('../views/ValoresCategoriaView.vue'),
+    },
+    {
+      path: '/valores-categoria/nuevo',
+      name: 'valor-categoria-nuevo',
+      component: () => import('../views/ValorCategoriaDetailView.vue'),
+    },
+    {
+      path: '/valores-categoria/:id',
+      name: 'valor-categoria-detalle',
+      component: () => import('../views/ValorCategoriaDetailView.vue'),
+    },
+    {
+      path: '/valores-fijos',
+      name: 'valores-fijos',
+      component: () => import('../views/ValoresFijosView.vue'),
+    },
     { path: '/asociacion-masiva', redirect: '/asociacion-masiva/valores-fijos' },
     {
       path: '/asociacion-masiva/valores-fijos',
       name: 'asociacion-masiva-valores-fijos',
-      component: AsociacionMasivaValoresFijosView,
+      component: () => import('../views/AsociacionMasivaValoresFijosView.vue'),
     },
     {
       path: '/asociacion-masiva/valores-categoria',
       name: 'asociacion-masiva-valores-categoria',
-      component: AsociacionMasivaValoresCategoriasView,
+      component: () => import('../views/AsociacionMasivaValoresCategoriasView.vue'),
     },
     {
       path: '/asociacion-masiva/conceptos',
       name: 'asociacion-masiva-conceptos',
-      component: AsociacionMasivaConceptosView,
+      component: () => import('../views/AsociacionMasivaConceptosView.vue'),
     },
     {
       path: '/clonacion-masiva/valores-fijos',
       name: 'clonacion-masiva-valores-fijos',
-      component: ClonacionMasivaValoresFijosView,
+      component: () => import('../views/ClonacionMasivaValoresFijosView.vue'),
     },
     {
       path: '/clonacion-masiva/valores-categoria',
       name: 'clonacion-masiva-valores-categoria',
-      component: ClonacionMasivaValoresCategoriaView,
+      component: () => import('../views/ClonacionMasivaValoresCategoriaView.vue'),
     },
     {
       path: '/clonacion-masiva/escala-salarial',
       name: 'actualizacion-masiva-escala-salarial',
-      component: ActualizacionMasivaEscalaSalarialView,
+      component: () => import('../views/ActualizacionMasivaEscalaSalarialView.vue'),
     },
-    { path: '/grupos-valor-fijo', name: 'grupos-valor-fijo', component: GruposValorFijoView },
-    { path: '/grupos-valor-categoria', name: 'grupos-valor-categoria', component: GruposValorCategoriaView },
+    {
+      path: '/grupos-valor-fijo',
+      name: 'grupos-valor-fijo',
+      component: () => import('../views/GruposValorFijoView.vue'),
+    },
+    {
+      path: '/grupos-valor-categoria',
+      name: 'grupos-valor-categoria',
+      component: () => import('../views/GruposValorCategoriaView.vue'),
+    },
   ],
 })
 
